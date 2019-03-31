a257e329687338f9b6515350340d1abcde73fede
King-Jason <jasoking2@gmail.com>
2019-04-01 00:02:45 +0800

    Initial commit

---
+++ ComputerGamesCollection.Designer.cs
@@ -0,0 +1,255 @@
+﻿namespace ComputerGamesCollection
+{
+    partial class ComputerGamesCollection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lbGameOutput = new System.Windows.Forms.ListBox();
+            this.tbGameNameInput = new System.Windows.Forms.TextBox();
+            this.tbGamePlatformInput = new System.Windows.Forms.TextBox();
+            this.tbGameGenreInput = new System.Windows.Forms.TextBox();
+            this.labelName = new System.Windows.Forms.Label();
+            this.labelPlatform = new System.Windows.Forms.Label();
+            this.labelGenre = new System.Windows.Forms.Label();
+            this.buttonAdd = new System.Windows.Forms.Button();
+            this.buttonClear = new System.Windows.Forms.Button();
+            this.buttonDelete = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonUpdate = new System.Windows.Forms.Button();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.buttonSave = new System.Windows.Forms.Button();
+            this.buttonOpen = new System.Windows.Forms.Button();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.buttonSaveAS = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lbGameOutput
+            // 
+            this.lbGameOutput.FormattingEnabled = true;
+            this.lbGameOutput.Location = new System.Drawing.Point(12, 12);
+            this.lbGameOutput.Name = "lbGameOutput";
+            this.lbGameOutput.Size = new System.Drawing.Size(308, 342);
+            this.lbGameOutput.TabIndex = 0;
+            this.lbGameOutput.SelectedIndexChanged += new System.EventHandler(this.lbGameOutput_SelectedIndexChanged);
+            // 
+            // tbGameNameInput
+            // 
+            this.tbGameNameInput.Location = new System.Drawing.Point(547, 32);
+            this.tbGameNameInput.Name = "tbGameNameInput";
+            this.tbGameNameInput.Size = new System.Drawing.Size(100, 20);
+            this.tbGameNameInput.TabIndex = 1;
+            this.toolTip.SetToolTip(this.tbGameNameInput, "Please enter a game name.");
+            // 
+            // tbGamePlatformInput
+            // 
+            this.tbGamePlatformInput.Location = new System.Drawing.Point(547, 58);
+            this.tbGamePlatformInput.Name = "tbGamePlatformInput";
+            this.tbGamePlatformInput.Size = new System.Drawing.Size(100, 20);
+            this.tbGamePlatformInput.TabIndex = 2;
+            this.toolTip.SetToolTip(this.tbGamePlatformInput, "Please enter a platform for the game.");
+            // 
+            // tbGameGenreInput
+            // 
+            this.tbGameGenreInput.Location = new System.Drawing.Point(547, 84);
+            this.tbGameGenreInput.Name = "tbGameGenreInput";
+            this.tbGameGenreInput.Size = new System.Drawing.Size(100, 20);
+            this.tbGameGenreInput.TabIndex = 3;
+            this.toolTip.SetToolTip(this.tbGameGenreInput, "Please enter a genre for the game.");
+            // 
+            // labelName
+            // 
+            this.labelName.AutoSize = true;
+            this.labelName.Location = new System.Drawing.Point(488, 35);
+            this.labelName.Name = "labelName";
+            this.labelName.Size = new System.Drawing.Size(38, 13);
+            this.labelName.TabIndex = 4;
+            this.labelName.Text = "Name:";
+            // 
+            // labelPlatform
+            // 
+            this.labelPlatform.AutoSize = true;
+            this.labelPlatform.Location = new System.Drawing.Point(488, 61);
+            this.labelPlatform.Name = "labelPlatform";
+            this.labelPlatform.Size = new System.Drawing.Size(48, 13);
+            this.labelPlatform.TabIndex = 5;
+            this.labelPlatform.Text = "Platform:";
+            // 
+            // labelGenre
+            // 
+            this.labelGenre.AutoSize = true;
+            this.labelGenre.Location = new System.Drawing.Point(488, 87);
+            this.labelGenre.Name = "labelGenre";
+            this.labelGenre.Size = new System.Drawing.Size(39, 13);
+            this.labelGenre.TabIndex = 6;
+            this.labelGenre.Text = "Genre:";
+            // 
+            // buttonAdd
+            // 
+            this.buttonAdd.Location = new System.Drawing.Point(491, 126);
+            this.buttonAdd.Name = "buttonAdd";
+            this.buttonAdd.Size = new System.Drawing.Size(75, 23);
+            this.buttonAdd.TabIndex = 7;
+            this.buttonAdd.Text = "Add";
+            this.buttonAdd.UseVisualStyleBackColor = true;
+            this.buttonAdd.Click += new System.EventHandler(this.buttonAdd_Click);
+            // 
+            // buttonClear
+            // 
+            this.buttonClear.Location = new System.Drawing.Point(572, 126);
+            this.buttonClear.Name = "buttonClear";
+            this.buttonClear.Size = new System.Drawing.Size(75, 23);
+            this.buttonClear.TabIndex = 8;
+            this.buttonClear.Text = "Clear";
+            this.buttonClear.UseVisualStyleBackColor = true;
+            this.buttonClear.Click += new System.EventHandler(this.buttonClear_Click);
+            // 
+            // buttonDelete
+            // 
+            this.buttonDelete.Location = new System.Drawing.Point(491, 155);
+            this.buttonDelete.Name = "buttonDelete";
+            this.buttonDelete.Size = new System.Drawing.Size(75, 23);
+            this.buttonDelete.TabIndex = 9;
+            this.buttonDelete.Text = "Delete";
+            this.buttonDelete.UseVisualStyleBackColor = true;
+            this.buttonDelete.Click += new System.EventHandler(this.buttonDelete_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(572, 155);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 10;
+            this.buttonReset.Text = "Reset";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // buttonUpdate
+            // 
+            this.buttonUpdate.Location = new System.Drawing.Point(491, 184);
+            this.buttonUpdate.Name = "buttonUpdate";
+            this.buttonUpdate.Size = new System.Drawing.Size(75, 23);
+            this.buttonUpdate.TabIndex = 11;
+            this.buttonUpdate.Text = "Update";
+            this.buttonUpdate.UseVisualStyleBackColor = true;
+            this.buttonUpdate.Click += new System.EventHandler(this.buttonUpdate_Click);
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(572, 184);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 12;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            this.buttonSearch.Click += new System.EventHandler(this.buttonSearch_Click);
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.Location = new System.Drawing.Point(491, 239);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(75, 23);
+            this.buttonSave.TabIndex = 13;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            this.buttonSave.Click += new System.EventHandler(this.buttonSave_Click);
+            // 
+            // buttonOpen
+            // 
+            this.buttonOpen.Location = new System.Drawing.Point(572, 239);
+            this.buttonOpen.Name = "buttonOpen";
+            this.buttonOpen.Size = new System.Drawing.Size(75, 23);
+            this.buttonOpen.TabIndex = 14;
+            this.buttonOpen.Text = "Open";
+            this.buttonOpen.UseVisualStyleBackColor = true;
+            this.buttonOpen.Click += new System.EventHandler(this.buttonOpen_Click);
+            // 
+            // toolTip
+            // 
+            this.toolTip.IsBalloon = true;
+            // 
+            // buttonSaveAS
+            // 
+            this.buttonSaveAS.Location = new System.Drawing.Point(532, 268);
+            this.buttonSaveAS.Name = "buttonSaveAS";
+            this.buttonSaveAS.Size = new System.Drawing.Size(75, 23);
+            this.buttonSaveAS.TabIndex = 15;
+            this.buttonSaveAS.Text = "Save As";
+            this.buttonSaveAS.UseVisualStyleBackColor = true;
+            this.buttonSaveAS.Click += new System.EventHandler(this.buttonSaveAS_Click);
+            // 
+            // ComputerGamesCollection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(926, 371);
+            this.Controls.Add(this.buttonSaveAS);
+            this.Controls.Add(this.buttonOpen);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.buttonUpdate);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonDelete);
+            this.Controls.Add(this.buttonClear);
+            this.Controls.Add(this.buttonAdd);
+            this.Controls.Add(this.labelGenre);
+            this.Controls.Add(this.labelPlatform);
+            this.Controls.Add(this.labelName);
+            this.Controls.Add(this.tbGameGenreInput);
+            this.Controls.Add(this.tbGamePlatformInput);
+            this.Controls.Add(this.tbGameNameInput);
+            this.Controls.Add(this.lbGameOutput);
+            this.Name = "ComputerGamesCollection";
+            this.Text = "Computer Games Collection";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.ComputerGamesCollection_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox lbGameOutput;
+        private System.Windows.Forms.TextBox tbGameNameInput;
+        private System.Windows.Forms.TextBox tbGamePlatformInput;
+        private System.Windows.Forms.TextBox tbGameGenreInput;
+        private System.Windows.Forms.Label labelName;
+        private System.Windows.Forms.Label labelPlatform;
+        private System.Windows.Forms.Label labelGenre;
+        private System.Windows.Forms.Button buttonAdd;
+        private System.Windows.Forms.Button buttonClear;
+        private System.Windows.Forms.Button buttonDelete;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonUpdate;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Button buttonSave;
+        private System.Windows.Forms.Button buttonOpen;
+        private System.Windows.Forms.ToolTip toolTip;
+        private System.Windows.Forms.Button buttonSaveAS;
+    }
+}
+
---
+++ ComputerGamesCollection.cs
@@ -0,0 +1,398 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Diagnostics;
+
+namespace ComputerGamesCollection
+{
+    //Name: Jason King
+    //Date: 28/03/2019
+    //ID: P465642
+    /*
+        ComputerGamesCollection
+        This program can keep track of all your games. You can add them to the 
+        list, delete, update and more. All data is saved to a binary file so
+        you don't have to enter the infomation in all the time.
+    */
+
+    public partial class ComputerGamesCollection : Form
+    {
+
+        static int MAX = 20;
+        string[] gameNameArray = new string[MAX];
+        string[] gamePlatformArray = new string[MAX];
+        string[] gameGenreArray = new string[MAX];
+        int PTR = 0;
+        string filePath = "myGames.dat";
+
+        public ComputerGamesCollection()
+        {
+            InitializeComponent();
+            onRunLoadFileData();
+        }
+
+        /*
+        Program Criteria 1: The program loads the data from mygames.dat 
+        if there is one on startup. If there isn't then it just 
+        populates the program with empty values.
+        */
+        private void onRunLoadFileData()
+        {
+            Open();
+        }
+
+        private void initArray()
+        {
+            for (int i = 0; i < MAX; i++)
+            {
+                gameNameArray[i] = "~empty";
+                gamePlatformArray[i] = "";
+                gameGenreArray[i] = "";
+            }
+            displayArray();
+        }
+
+        /*
+        Program Criteria 2: When all the textbox fields have valid data in 
+        them it adds the value to the three arrays 
+        and then displays it in the listbox.
+        */
+        private void buttonAdd_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbGameNameInput.Text) || string.IsNullOrWhiteSpace(tbGamePlatformInput.Text) || string.IsNullOrWhiteSpace(tbGameGenreInput.Text))
+            {
+                MessageBox.Show("Please fill in all fields");
+            }
+            else
+            {
+                insertDataArray();
+                clearTextBoxes();
+            }
+        }
+
+        //Adds the data from the textboxes to the array
+        private void insertDataArray()
+        {
+            try
+            {
+                gameNameArray[PTR] = tbGameNameInput.Text;
+                gamePlatformArray[PTR] = tbGamePlatformInput.Text;
+                gameGenreArray[PTR] = tbGameGenreInput.Text;
+                PTR++;
+                bubbleSort();
+                displayArray();
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Your list is full. \nPlease change an already existing game.\n"  + e);
+            }
+
+        }
+
+        //Clears the textboxes
+        private void clearTextBoxes()
+        {
+            tbGameNameInput.Clear();
+            tbGamePlatformInput.Clear();
+            tbGameGenreInput.Clear();
+        }
+
+        /*
+        Program Criteria 3: Deletes the record that is selected in the listbox, 
+        clears the textboxes after and then sorts the the array.
+        */
+        private void buttonDelete_Click(object sender, EventArgs e)
+        {
+            if (lbGameOutput.Items.Count > 0)
+            {
+                PTR = lbGameOutput.SelectedIndex;
+                gameNameArray[lbGameOutput.SelectedIndex] = "~empty";
+                gamePlatformArray[lbGameOutput.SelectedIndex] = "";
+                gameGenreArray[lbGameOutput.SelectedIndex] = "";
+            }
+            else
+            {
+                MessageBox.Show("Please select something to delete.");
+            }
+            bubbleSort();
+            displayArray();
+            clearTextBoxes();
+        }
+
+        /*
+        Deletes all data and records in the program.
+        */
+        private void buttonClear_Click(object sender, EventArgs e)
+        {
+            initArray();
+            lbGameOutput.Items.Clear();
+            clearTextBoxes();
+            displayArray();
+        }
+
+        /*
+        Program Criteria 4: You can select a record from the listbox and type in 
+        different value and click update and it will change the record name 
+        and sort the array
+        */
+        private void buttonUpdate_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(tbGameNameInput.Text) || string.IsNullOrWhiteSpace(tbGamePlatformInput.Text) || string.IsNullOrWhiteSpace(tbGameGenreInput.Text))
+            {
+                MessageBox.Show("Please fill in all fields");
+            }
+            else
+            {
+                gameNameArray[lbGameOutput.SelectedIndex] = tbGameNameInput.Text;
+                gamePlatformArray[lbGameOutput.SelectedIndex] = tbGamePlatformInput.Text;
+                gameGenreArray[lbGameOutput.SelectedIndex] = tbGameGenreInput.Text;
+                int value = lbGameOutput.SelectedIndex;
+                bubbleSort();
+                displayArray();
+            }
+        }
+
+        //Program Criteria 6: Populates the textboxes with what record has been selected in the listbox
+        private void lbGameOutput_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            clearTextBoxes();
+            int value = lbGameOutput.SelectedIndex;
+            if (value >= 0 && value <= 19)
+            {
+                tbGameNameInput.Text += gameNameArray[value];
+                tbGamePlatformInput.Text += gamePlatformArray[value];
+                tbGameGenreInput.Text += gameGenreArray[value];
+            }
+        }
+
+        //Program Criteria 10: Writes all the data from the arrays to a binary file when the button save is clicked
+        private void buttonSave_Click(object sender, EventArgs e)
+        {
+            filePath = "mygames.dat";
+            Save();
+        }
+
+        private void buttonOpen_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog openFileDialog = new OpenFileDialog())
+            {
+                openFileDialog.InitialDirectory = "c:\\";
+                openFileDialog.Filter = "dat files (*.dat)|*.dat|All files (*.*)|*.*";
+                openFileDialog.FilterIndex = 2;
+                openFileDialog.RestoreDirectory = true;
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = openFileDialog.FileName;
+                }
+            }
+            Open();
+        }
+
+        /*
+        Program Criteria 5: When the reset button is clicked it changes 
+        whatever the selected record is too empty and sorts the arrays.
+        */
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            if (lbGameOutput.SelectedIndex != -1)
+            {
+                gameNameArray[lbGameOutput.SelectedIndex] = "~empty";
+                gamePlatformArray[lbGameOutput.SelectedIndex] = "";
+                gameGenreArray[lbGameOutput.SelectedIndex] = "";
+                bubbleSort();
+                displayArray();
+            }
+            else
+            {
+                MessageBox.Show("Please select a record to reset!");
+            }
+
+        }
+
+        /*
+        Program Criteria 8: If there is a game name in the textbox when the search button 
+        is clicked it will search the gameNameArray for the game then fill the rest of 
+        the boxes with the infomation.
+        */
+        private void buttonSearch_Click(object sender, EventArgs e)
+        {
+            binarySearch();
+        }
+
+        private void binarySearch()
+        {
+            if (string.IsNullOrWhiteSpace(tbGameNameInput.Text))
+            {
+                MessageBox.Show("Please enter a word to search");
+            }
+            else
+            {
+                bubbleSort();
+                bool found = false;
+                int min = 0;
+                int max = MAX - 1;
+                string target = tbGameNameInput.Text;
+                while (min <= max)
+                {
+                    int mid = ((min + max) / 2);
+                    if (string.CompareOrdinal(target, gameNameArray[mid]) == 0)
+                    {
+                        found = true;
+                        lbGameOutput.SetSelected(mid, true);
+                        break;
+                    }
+                    else if (string.CompareOrdinal(target, gameNameArray[mid]) < 0)
+                    {
+                        max = mid - 1;
+                    }
+                    else
+                    {
+                        min = mid + 1;
+                    }
+
+                    ;
+                }// end of WHILE
+                if (!found)
+                {
+                    MessageBox.Show("Could not find " + target);
+                    clearTextBoxes();
+                }
+
+            }
+        }
+
+        //Program Criteria 7: Sorts the game names by alphabetically order using a bubblesort
+        private void bubbleSort()
+        {
+            int pos = 1;
+            while (pos < gameNameArray.Length)
+            {
+                if (string.CompareOrdinal(gameNameArray[pos], gameNameArray[pos - 1]) >= 0 || gameNameArray[pos] == "~empty")
+                {
+                    pos++;
+                }
+                else
+                {
+
+                    arrayAlphabeticSort(gameNameArray, pos);
+                    arrayAlphabeticSort(gamePlatformArray, pos);
+                    arrayAlphabeticSort(gameGenreArray, pos);
+
+                    if (pos > 1)
+                    {
+                        pos--;
+                    }
+                }
+            }
+        }
+
+        private void arrayAlphabeticSort(String[] array, int pos)
+        {
+            string temp;
+            temp = array[pos];
+            array[pos] = array[pos - 1];
+            array[pos - 1] = temp;
+        }
+
+        //Displays all the data in the arrays in the listbox
+        private void displayArray()
+        {
+            lbGameOutput.Items.Clear();
+            for (int i = 0; i < MAX; i++)
+            {
+                lbGameOutput.Items.Add(gameNameArray[lbGameOutput.Items.Count] + " " + gamePlatformArray[lbGameOutput.Items.Count] + " " + gameGenreArray[lbGameOutput.Items.Count]);
+            }
+        }
+
+        /*
+        Program Criteria 9: When the user exits the problem before it closes all data is saved to mygames.dat
+        */
+        private void ComputerGamesCollection_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            filePath = "myGames.dat";
+            Save();
+        }
+
+        private void Save()
+        {
+            try
+            {
+                BinaryWriter bw = new BinaryWriter(new FileStream(filePath, FileMode.Create));
+                for (int i = 0; i < MAX; i++)
+                {
+                    bw.Write(gameNameArray[i]);
+                    bw.Write(gamePlatformArray[i]);
+                    bw.Write(gameGenreArray[i]);
+
+                }
+                bw.Close();
+                MessageBox.Show("Successfully Saved");
+            }
+            catch (Exception fe)
+            {
+                MessageBox.Show("There was an error saving! \n" + fe);
+            }
+        }
+
+        private void Open()
+        {
+            try
+            {
+                BinaryReader br = new BinaryReader(new FileStream(filePath, FileMode.Open));
+                for (int i = 0; i < MAX; i++)
+                {
+                    gameNameArray[i] = br.ReadString();
+                    gamePlatformArray[i] = br.ReadString();
+                    gameGenreArray[i] = br.ReadString();
+                }
+                br.Close();
+            }
+            catch (Exception fe)
+            {
+                if (File.Exists("mygames.dat"))
+                {
+                    MessageBox.Show("There was an error opening your file \n" + fe);
+                    initArray();
+                }
+                else
+                {
+                    initArray();
+                }
+
+            }
+            bubbleSort();
+            displayArray();
+
+            for (int i = 0; i < 20; i++)
+            {
+                if (gameNameArray[i] != "~empty")
+                {
+                    PTR++;
+                }
+            }
+        }
+
+        private void buttonSaveAS_Click(object sender, EventArgs e)
+        {
+            using(FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog()) {
+                if (folderBrowserDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filePath = folderBrowserDialog.SelectedPath;
+                    filePath += "mygames.dat";
+                }
+            }
+            Save();
+        }
+    }
+
+}
+
+
